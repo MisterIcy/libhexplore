638d63563b0cd929eab2b76060f92dc2126557d7
Alexandros Koutroulis <icyd3mon@gmail.com>
2024-09-28 15:03:36 +0300

    Initial IO Code

---
+++ tests/io/IO.gtest.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+#include <libhexplore/io/IO.hpp>
+#include <fstream>
+
+TEST(IO, OpenWhenFileExists) {
+    libhexplore::io::IO io;
+    // Create a file
+    std::string file = "test.txt";
+    std::fstream fs(file, std::ios::out);
+    fs.write("Hello, World!", 13);
+    fs.close();
+
+    // Open the file
+    EXPECT_TRUE(io.open(file));
+    EXPECT_TRUE(io.isOpen());
+
+    // Delete the file
+    std::remove(file.c_str());
+}
+
+TEST(IO, OpenWhenFileIsAlreadyOpened) {
+    libhexplore::io::IO io;
+    // Create a file
+    std::string file = "test.txt";
+    std::fstream fs(file, std::ios::out);
+    fs.write("Hello, World!", 13);
+    fs.close();
+
+    // Open the file
+    EXPECT_TRUE(io.open(file));
+    EXPECT_TRUE(io.isOpen());
+    // Try to open the file again
+    EXPECT_FALSE(io.open(file));
+    EXPECT_EQ(io.getLastError(), 1);
+
+    // Delete the file
+    std::remove(file.c_str());    
+}
+
+TEST(IO, OpenFileWhenFileDoesNotExist) {
+    libhexplore::io::IO io;
+    // Try to open a file that does not exist
+    EXPECT_FALSE(io.open("non_existent_file.txt"));
+    EXPECT_EQ(io.getLastError(), 2);
+}
